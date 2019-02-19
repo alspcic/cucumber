cb731594d75e784506b216e02ff9f5dc264e0de0
dainis.urbelis <dainis.urbelis@accenture.com>
2019-02-19 18:37:02 +0200

    new step file

---
+++ cucumber/src/test/java/stepDefinitions/TaskSteps.java
@@ -0,0 +1,29 @@
+package stepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+
+public class TaskSteps{
+    private WebDriver driver;
+
+    public TaskSteps() {  this.driver = Hooks.driver;  }
+
+
+
+
+
+
+}
