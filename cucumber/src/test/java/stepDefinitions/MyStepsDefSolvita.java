ba17cad21c775ada5cceacf56893db88c2b680e3
Solvita Vicupe <nallade@users.noreply.github.com>
2019-03-12 07:42:14 +0200

    minor change in import

--- cucumber/src/test/java/stepDefinitions/MyStepsDefSolvita.java
+++ cucumber/src/test/java/stepDefinitions/MyStepsDefSolvita.java
@@ -10,9 +10,7 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.Map;
-
 import static org.junit.Assert.assertEquals;
 
 public class MyStepsDefSolvita {
